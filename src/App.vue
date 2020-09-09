1f1c01c69f0c22ef2dc18a829af6c5e3bd89c859
limzykenneth <limzy.kenneth@gmail.com>
2020-09-09 20:25:01 +0100

    Prepare controls component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
 	<main id="page-content">
 		<app-canvas></app-canvas>
+		<app-controls></app-controls>
 	</main>
 </template>
 
 <script>
 import AppCanvas from "./components/Canvas.vue";
+import AppControls from "./components/Controls.vue";
 
 export default{
 	name: "App",
 	components: {
-		"app-canvas": AppCanvas
+		"app-canvas": AppCanvas,
+		"app-controls": AppControls
 	}
 };
 </script>
---
+++ src/components/Controls.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="controls-container"></div>
+</template>
+
+<script>
+export default{
+	name: "AppControls"
+};
+</script>
+
+<style lang="less" scoped>
+#controls-container{
+
+}
+</style>
