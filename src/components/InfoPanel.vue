73f08648d5c52f2f1b0152ab9a3551054a7d3b27
limzykenneth <limzy.kenneth@gmail.com>
2020-09-12 12:58:57 +0100

    Style info panel

--- src/components/InfoPanel.vue
+++ src/components/InfoPanel.vue
@@ -1,9 +1,23 @@
 <template>
 	<section id="info-container">
-		<p>Borough: {{ currentArea || "Greater London" }}</p>
-		<p>Date: {{ currentDate.format("Do MMMM YYYY") }}</p>
-		<p>New cases: {{ newCases !== false ? newCases : allNewCases }}</p>
-		<p>Total cases: {{ totalCases !== false ? totalCases : allTotalCases }}</p>
+		<table id="info-table">
+			<tr>
+				<td class="label">Borough</td>
+				<td class="data">{{ currentArea || "Greater London" }}</td>
+			</tr>
+			<tr>
+				<td class="label">Date</td>
+				<td class="data">{{ currentDate.format("Do MMMM YYYY") }}</td>
+			</tr>
+			<tr>
+				<td class="label">New Cases</td>
+				<td class="data">{{ newCases !== false ? newCases : allNewCases }}</td>
+			</tr>
+			<tr>
+				<td class="label">Total Cases</td>
+				<td class="data">{{ totalCases !== false ? totalCases : allTotalCases }}</td>
+			</tr>
+		</table>
 	</section>
 </template>
 
@@ -78,5 +92,24 @@ export default{
 	top: 0;
 	color: white;
 	margin: 20px;
+
+	#info-table{
+		table-layout: fixed;
+		border: 1px solid white;
+		border-collapse: collapse;
+
+		td{
+			padding: 10px;
+			border: 1px solid white;
+		}
+
+		.label{
+
+		}
+
+		.data{
+			min-width: 15rem;
+		}
+	}
 }
 </style>
